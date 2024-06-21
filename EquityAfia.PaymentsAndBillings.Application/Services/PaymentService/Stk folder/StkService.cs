0f013dd0e442f1e5aba3e893078c6e2627095a1e
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 13:11:56 +0300

    added aline of code

--- EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
@@ -76,6 +76,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Services.PaymentService.Stk
         }
         private class TokenResponse
         {
+            public string AccessToken { get; set; }
 
         }
 
