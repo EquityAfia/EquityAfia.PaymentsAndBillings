ed02284f1a1a476ad25e0771145bda431702acf2
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 11:54:33 +0300

    added StkService file

---
+++ EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquityAfia.PaymentsAndBillings.Application.Services.PaymentService.Stk_folder
+{
+    internal class StkService
+    {
+    }
+}
