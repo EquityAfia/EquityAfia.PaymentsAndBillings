a14cd1a8f1811c7d601bc50e9ba13b8130353189
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 11:35:29 +0300

    Added stkinterfase

---
+++ EquityAfia.PaymentsAndBillings.Application/Interfaces/Payments/Stk/IstkService.cs
@@ -0,0 +1,14 @@
+﻿using EquityAfia.PaymentsAndBillings.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquityAfia.PaymentsAndBillings.Application.Interfaces.Payments.Stk
+{
+    public interface  IStkService
+    {
+        Task<Payment> MakeStkPaymentAsync(int billingId, string mobileNumber);
+    }
+}
