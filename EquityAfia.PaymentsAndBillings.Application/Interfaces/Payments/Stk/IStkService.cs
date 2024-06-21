375fa1240e62ce20a9c4de824c4f401217dcb452
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 12:07:14 +0300

    _added anew line of code

---
+++ EquityAfia.PaymentsAndBillings.Application/Interfaces/Payments/Stk/IStkService.cs
@@ -0,0 +1,15 @@
+﻿using EquityAfia.PaymentsAndBillings.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquityAfia.PaymentsAndBillings.Application.Interfaces.Payments.Stk
+{
+    public interface  IStkService
+    {
+        Task<Payment> MakeStkPaymentAsync(int billingId, string mobileNumber);
+    }
+    _configuration = configuration;
+}
