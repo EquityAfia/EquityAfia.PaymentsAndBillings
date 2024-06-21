fbb1f71073c289f4e3e5a4690a33249635636e9d
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 10:13:55 +0300

    moved necessary stk related file to stk folder

---
+++ EquityAfia.PaymentsAndBillings.Contracts/Payment/Stk/StkPayment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquityAfia.PaymentsAndBillings.Contracts.Payment.Stk
+{
+    public class StkPayment
+    {
+        public string MobileNumber { get; set; }
+        public int BillingId { get; set; }
+    }
+}
