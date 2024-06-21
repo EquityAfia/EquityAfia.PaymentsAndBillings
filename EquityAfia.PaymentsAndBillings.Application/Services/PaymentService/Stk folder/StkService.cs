18259a019dde2511f0b664e02b17caa04e475915
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 13:18:30 +0300

    public string ResponseDescription { get; set; } line added

--- EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
@@ -84,7 +84,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Services.PaymentService.Stk
         {
             public string MerchantRequestID { get; set; }
             public string CheckoutRequestID { get; set; }
-            public string ResponseCode { get; set; 
+            public string ResponseCode { get; set; }
 
 
         }
