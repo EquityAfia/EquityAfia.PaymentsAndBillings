e33f25317324147d7c3d76e2928a8157994c55dd
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-21 12:18:48 +0300

    solved one error

--- EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
@@ -23,3 +23,4 @@ namespace EquityAfia.PaymentsAndBillings.Application.Services.PaymentService.Stk
         {
             var billing = await _context.Billings.FindAsync(billingId);
         }
+    }
