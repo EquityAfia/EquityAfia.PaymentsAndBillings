ce487da77ea8ca0d93e151c38321c1d965a8487d
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-23 21:40:37 +0300

    trying to solve an error in service stk payment file

--- EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
@@ -26,7 +26,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Services.PaymentService.Stk
 
         public async Task<Payment> MakeStkPaymentAsync(int billingId, string mobileNumber)
         {
-             billing = await _billingRepository.GetBillingByIdAsync(billingId);
+             _billing = await _billingRepository.GetBillingByIdAsync(billingId);
             if (billing == null)
             {
                 throw new Exception("Billing not found");
