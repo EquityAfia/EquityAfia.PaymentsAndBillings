146db5dbead4232f3f02cfa4de498f9d83b3ffdd
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-23 21:14:48 +0300

    trying to solve an error in service stk payment file

--- EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/PaymentService/Stk folder/StkService.cs
@@ -79,7 +79,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Services.PaymentService.Stk
                 TransactionId = transactionId,
                 PaymentStatus = "Paid"
             };
-            _paymentRepository.Add(payment); // This line may need to change depending on your repository pattern
+            IPaymentRepository.Add(payment); // This line may need to change depending on your repository pattern
             await _paymentRepository.SaveChangesAsync(); // This line may need to change depending on your repository pattern
 
             return payment;
