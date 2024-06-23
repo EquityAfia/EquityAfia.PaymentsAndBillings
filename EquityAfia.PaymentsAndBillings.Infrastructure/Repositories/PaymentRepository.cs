2cddf9cf44167b87f50315ac5780bbdcc55911ca
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-23 21:49:27 +0300

    trying to solve an error in service stk payment file

--- EquityAfia.PaymentsAndBillings.Infrastructure/Repositories/PaymentRepository.cs
+++ EquityAfia.PaymentsAndBillings.Infrastructure/Repositories/PaymentRepository.cs
@@ -9,7 +9,7 @@ using EquityAfia.PaymentsAndBillings.Application.Interfaces;
 
 namespace EquityAfia.PaymentsAndBillings.Infrastructure.Repositories
 {
-    public class PaymentRepository :IPaymentRep
+    public class PaymentRepository :IPaymentRepo
     {
         private readonly EquityAfiaDbContext _context;
 
