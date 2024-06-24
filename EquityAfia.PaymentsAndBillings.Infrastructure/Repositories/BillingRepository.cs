cf6d6fc68f17fcaf34b055a39070d8be7431a37c
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-24 10:27:49 +0300

    modified to solve error

--- EquityAfia.PaymentsAndBillings.Infrastructure/Repositories/BillingRepository.cs
+++ EquityAfia.PaymentsAndBillings.Infrastructure/Repositories/BillingRepository.cs
@@ -7,7 +7,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Repositories
 {
     public class BillingRepository : IBillingRepository
     {
-        private readonly AppDbContext _context;
+        private readonly ApDbContext _context;
 
         public BillingRepository(ApplicationDbContext context)
         {
