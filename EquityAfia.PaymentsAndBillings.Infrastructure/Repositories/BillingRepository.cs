42e1ce61000ce604579bbcdb91f2f0ba2f0ed10d
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-24 10:30:47 +0300

    modified to solve error

--- EquityAfia.PaymentsAndBillings.Infrastructure/Repositories/BillingRepository.cs
+++ EquityAfia.PaymentsAndBillings.Infrastructure/Repositories/BillingRepository.cs
@@ -9,7 +9,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Repositories
     {
         private readonly DbContext _context;
 
-        public BillingRepository(ationDbContext context)
+        public BillingRepository(tionDbContext context)
         {
             _context = context;
         }
