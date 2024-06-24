67abaeb6146c268b3ed904a9cbd8b3ae8d490fac
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-24 11:29:34 +0300

    solved one error

--- EquityAfia.PaymentsAndBillings.Application/Interfaces/BillingsInterface/IBillingRepository.cs
+++ EquityAfia.PaymentsAndBillings.Application/Interfaces/BillingsInterface/IBillingRepository.cs
@@ -7,6 +7,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Interfaces.Billing
     public interface IBillingRepository
     {
         Task AddAsync(BillingDto billing); // Add a billing entity
+        Task AddAsync(Domain.Entities.Billing billing);
         Task<BillingDto> GetBillingByIdAsync(int billingId); // Get a billing entity by ID
     }
 }
