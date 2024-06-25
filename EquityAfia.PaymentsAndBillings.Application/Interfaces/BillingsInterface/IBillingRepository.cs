1a4456fd531a84e97d6528dc8718c72aedb3a48f
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-25 15:32:02 +0300

    trying to solve an error

--- EquityAfia.PaymentsAndBillings.Application/Interfaces/BillingsInterface/IBillingRepository.cs
+++ EquityAfia.PaymentsAndBillings.Application/Interfaces/BillingsInterface/IBillingRepository.cs
@@ -2,7 +2,7 @@
 {
     public interface IBillingRepository
     {
-        Task AddAsync( IBillingRepository,IBillin);
+        Task AddAsync( IBillingRepository,IBilling);
         Task<Billing> GetBillingByIdOrAppointmentIdAsync(int billingId, int appointmentId);
         Task<Billing> GetBillingByIdAsync(int billingId);
     }
