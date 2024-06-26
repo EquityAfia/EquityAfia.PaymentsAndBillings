bd8886fe2186c546e1c2340c8589e1dae7334ab4
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-26 09:33:38 +0300

    trying to solve an error

--- EquityAfia.PaymentsAndBillings.Infrastructure/DependencyInjection.cs
+++ EquityAfia.PaymentsAndBillings.Infrastructure/DependencyInjection.cs
@@ -37,7 +37,7 @@ namespace EquityAfia.PaymentsAndBillings.Infrastructure
             services.AddScoped<IStkService, StkService>();
 
             // Register AutoMapper with fully qualified method call
-            AutoMapper.Extensions.Microsoft.DependencyInjection.ServiceCollectionExtensions.AddAutoMapper(
+            .AddAutoMapper(
                 services,
                 typeof(EquityAfia.PaymentsAndBillings.Application.Mappings.MappingProfile).Assembly
             );
