e30da881026ff375296e5d78b16a20af1dbd500b
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-28 17:02:08 +0300

    trying to sopve an error

--- EquityAfia.PaymentsAndBillings.Infrastructure/Mapping/MapsterMapperImpl.cs
+++ EquityAfia.PaymentsAndBillings.Infrastructure/Mapping/MapsterMapperImpl.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using EquityAfia.PaymentsAndBillings.Contracts.Billing;
+using EquityAfia.PaymentsAndBillings.Domain.Entities;
 using Mapster;
 
 namespace EquityAfia.PaymentsAndBillings.Infrastructure.Mapping
@@ -10,38 +12,37 @@ namespace EquityAfia.PaymentsAndBillings.Infrastructure.Mapping
         public MapsterMapperImpl(TypeAdapterConfig config)
         {
             _config = config;
+            ConfigureMappings(_config);
         }
 
-        public TDestination Map<TDestination>(object source)
+        private static void ConfigureMappings(TypeAdapterConfig config)
         {
-            return source.Adapt<TDestination>(_config);
+            // Configure global settings if needed
+            TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.Flexible);
+
+            // Example of mapping configuration between types
+            config.NewConfig<BillingDto, Billing>()
+                .Map(dest => dest.BillingId, src => src.BillingId)
+                .Map(dest => dest.AmountBilled, src => src.AmountBilled)
+                .Map(dest => dest.CustomerId, src => src.CustomerId)
+                .Map(dest => dest.CustomerName, src => src.CustomerName)
+                .Map(dest => dest.CustomerEmail, src => src.CustomerEmail)
+                .Map(dest => dest.Products, src => src.Products)
+                .Map(dest => dest.Services, src => src.Services);
         }
 
-        public TDestination Map<TSource, TDestination>(TSource source)
-        {
-            return source.Adapt<TSource, TDestination>(_config);
-        }
+        public TDestination Map<TDestination>(object source) => source.Adapt<TDestination>(_config);
 
-        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
-        {
-            return source.Adapt(destination, _config);
-        }
+        public TDestination Map<TSource, TDestination>(TSource source) => source.Adapt<TSource, TDestination>(_config);
 
-        public object Map(object source, Type sourceType, Type destinationType)
-        {
-            return source.Adapt(sourceType, destinationType, _config);
-        }
+        public TDestination Map<TSource, TDestination>(TSource source, TDestination destination) => source.Adapt(destination, _config);
 
-        public object Map(object source, object destination, Type sourceType, Type destinationType)
-        {
-            return source.Adapt(destination, sourceType, destinationType, _config);
-        }
+        public object Map(object source, Type sourceType, Type destinationType) => source.Adapt(sourceType, destinationType, _config);
+
+        public object Map(object source, object destination, Type sourceType, Type destinationType) => source.Adapt(destination, sourceType, destinationType, _config);
 
         public TypeAdapterConfig Config => _config;
 
-        public TypeAdapterBuilder<TSource> From<TSource>(TSource source)
-        {
-            return _config.From(source);
-        }
+        public TypeAdapterBuilder<TSource> From<TSource>(TSource source) => TypeAdapterBuilder<TSource>.New().Map(_config);
     }
 }
