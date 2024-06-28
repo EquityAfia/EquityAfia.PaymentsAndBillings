fb5962dd909fd8bccd2d82e797d8524099ec8990
Gladysrono <gladyschepkoech408@gmail.com>
2024-06-28 09:43:52 +0300

    tring to solve an error

--- EquityAfia.PaymentsAndBillings.Infrastructure/Mapping/MapsterMapper.cs
+++ EquityAfia.PaymentsAndBillings.Infrastructure/Mapping/MapsterMapper.cs
@@ -54,68 +54,68 @@ using EquityAfia.PaymentsAndBillings.Domain.Entities;
 using Mapster;
 
 namespace EquityAfia.PaymentsAndBillings.Infrastructure.Mapping
+{
+    public static class MapsterMapper
     {
-        public static class MapsterMapper
+        private static readonly TypeAdapterConfig _config;
+
+        static MapsterMapper()
         {
-            private static readonly TypeAdapterConfig _config;
+            _config = new TypeAdapterConfig();
+            // Configure mappings
+            ConfigureMappings(_config);
+        }
 
-            static MapsterMapper()
-            {
-                _config = new TypeAdapterConfig();
-                // Configure mappings
-                ConfigureMappings(_config);
-            }
+        public static void ConfigureMappings(TypeAdapterConfig config)
+        {
+            // Example of configuring a mapping
+            TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.Flexible);
 
-            public static void ConfigureMappings(TypeAdapterConfig config)
-            {
-                // Example of configuring a mapping
-                TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.Flexible);
+            // Example of mapping configuration between types
+            config.ForType<BillingDto, Billing>()
+                .Map(dest => dest.BillingId, src => src.BillingId)
+                .Map(dest => dest.AmountBilled, src => src.AmountBilled)
+                .Map(dest => dest.CustomerId, src => src.CustomerId)
+                .Map(dest => dest.CustomerName, src => src.CustomerName)
+                .Map(dest => dest.CustomerEmail, src => src.CustomerEmail)
+                .Map(dest => dest.Products, src => src.Products)
+                .Map(dest => dest.Services, src => src.Services);
+        }
 
-                // Example of mapping configuration between types
-                config.ForType<BillingDto, Billing>()
-                    .Map(dest => dest.BillingId, src => src.BillingId)
-                    .Map(dest => dest.AmountBilled, src => src.AmountBilled)
-                    .Map(dest => dest.CustomerId, src => src.CustomerId)
-                    .Map(dest => dest.CustomerName, src => src.CustomerName)
-                    .Map(dest => dest.CustomerEmail, src => src.CustomerEmail)
-                    .Map(dest => dest.Products, src => src.Products)
-                    .Map(dest => dest.Services, src => src.Services);
-            }
+        public static TDestination Map<TDestination>(object source)
+        {
+            return source.Adapt<TDestination>(_config);
+        }
 
-            public static TDestination Map<TDestination>(object source)
-            {
-                return source.Adapt<TDestination>(_config);
-            }
+        public static TDestination Map<TSource, TDestination>(TSource source)
+        {
+            return source.Adapt<TSource, TDestination>(_config);
+        }
 
-            public static TDestination Map<TSource, TDestination>(TSource source)
-            {
-                return source.Adapt<TSource, TDestination>(_config);
-            }
+        public static TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
+        {
+            return source.Adapt(destination, _config);
+        }
 
-            public static TDestination Map<TSource, TDestination>(TSource source, TDestination destination)
-            {
-                return source.Adapt(destination, _config);
-            }
+        public static object Map(object source, Type sourceType, Type destinationType)
+        {
+            return source.Adapt(destinationType, _config);
+        }
 
-            public static object Map(object source, Type sourceType, Type destinationType)
-            {
-                return source.Adapt(destinationType, _config);
-            }
+        public static object Map(object source, object destination, Type sourceType, Type destinationType)
+        {
+            return source.Adapt(destination, sourceType, destinationType, _config);
+        }
 
-            public static object Map(object source, object destination, Type sourceType, Type destinationType)
-            {
-                return source.Adapt(destination, sourceType, destinationType, _config);
-            }
-
-            public static TypeAdapterBuilder<TSource, TDestination> From<TSource, TDestination>(TSource source)
-            {
-                return _config.NewConfig<TSource, TDestination>().Compile();
-            }
+        public static TypeAdapterBuilder<TSource, TDestination> From<TSource, TDestination>(TSource source)
+        {
+            return _config.NewConfig<TSource, TDestination>().Compile();
         }
     }
+}
 
 
-    public static ITypeAdapterBuilder<TSource, TDestination> From<TSource, TDestination>(TSource source)
+        public static ITypeAdapterBuilder<TSource, TDestination> From<TSource, TDestination>(TSource source)
         {
             return _config.ForType<TSource, TDestination>();
         }
