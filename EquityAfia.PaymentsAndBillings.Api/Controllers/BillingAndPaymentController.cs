0e15d6bec67486e8024b04ae565611621229eeb4
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-01 12:59:13 +0300

    trying to solveerrors in the controller class

--- EquityAfia.PaymentsAndBillings.Api/Controllers/BillingAndPaymentController.cs
+++ EquityAfia.PaymentsAndBillings.Api/Controllers/BillingAndPaymentController.cs
@@ -36,8 +36,7 @@ namespace YourNamespace.Controllers
             try
             {
                 var billings = await _billingRepository.GetAllBillingsAsync();
-               if (billings == null || billings.Count == 0)
-
+                iif(billings == null || billings.Count == 0)
 
                     return NotFound(new { message = "No billings found" });
 
