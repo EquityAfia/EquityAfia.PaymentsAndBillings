374799a05742c5014b9a9b43f9b5613f7d2ccdd0
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 16:11:51 +0300

    modified a line of code

--- EquityAfia.PaymentsAndBillings.Contracts/Messages/CommodityMedicineManagement/GetProductDetailsRequest.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Messages/CommodityMedicineManagement/GetProductDetailsRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EquityAfia.PaymentsAndBillings.Contracts.Messages.CommodityMedicineManagement
 {
-    interna class GetProductDetailsRequest
+    intern class GetProductDetailsRequest
     {
         public string ProductId { get; set; }
 
