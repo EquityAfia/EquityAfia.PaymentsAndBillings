8c2dbe68a28dc955f6abeaf861563397ee3a10f9
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 15:40:51 +0300

    modified a code line

--- EquityAfia.PaymentsAndBillings.Contracts/Messages/AppointmentBookings/GetAppointmentDetailsResponse.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Messages/AppointmentBookings/GetAppointmentDetailsResponse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EquityAfia.PaymentsAndBillings.Contracts.Messages.AppointmentBookings
 {
-  publ   class GetAppointmentDetailsResponse
+  publi   class GetAppointmentDetailsResponse
     {
     }
 }
