721549b75cece6abaf307d4358aceff701b645cf
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 15:30:50 +0300

    modified aline of code

--- EquityAfia.PaymentsAndBillings.Contracts/Messages/AppointmentBookings/GetAppointmentDetailsRequest.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Messages/AppointmentBookings/GetAppointmentDetailsRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EquityAfia.PaymentsAndBillings.Contracts.Messages.AppointmentBookings
 {
-    interna class GetAppointmentDetailsRequest
+    intern class GetAppointmentDetailsRequest
     {
     }
 }
