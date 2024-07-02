8b930b8db239a20012f466ad3998ad7904d4e8e6
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 15:25:22 +0300

    added anew line of ciode

--- EquityAfia.PaymentsAndBillings.Contracts/Messages/CommodityMedicineManagement/GetProductDetailsResponse.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Messages/CommodityMedicineManagement/GetProductDetailsResponse.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EquityAfia.PaymentsAndBillings.Contracts.Messages.CommodityMedicineManagement
 {
-    publi class GetProductDetailsResponse
+    public class GetProductDetailsResponse
     {
         public string ProductId { get; set; }
     }
