56b0b730bf2e9b8f88a6fdaff1dbc4336ed586f5
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 16:05:49 +0300

    modified a line of code

--- EquityAfia.PaymentsAndBillings.Contracts/Messages/UserManagement/GetUserDetailsRequest.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Messages/UserManagement/GetUserDetailsRequest.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EquityAfia.PaymentsAndBillings.Contracts.Messages.UserManagement
 {
-    pub class GetUserDetailsRequest
+    publ class GetUserDetailsRequest
     {
         public int UserId { get; set; }
     }
