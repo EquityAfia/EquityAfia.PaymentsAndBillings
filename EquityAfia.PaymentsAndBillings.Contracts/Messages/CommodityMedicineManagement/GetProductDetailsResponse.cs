274ee457946108d6fcd9dd33fca2f1664fc37d13
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-02 15:27:35 +0300

    added anew line of ciode

--- EquityAfia.PaymentsAndBillings.Contracts/Messages/CommodityMedicineManagement/GetProductDetailsResponse.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Messages/CommodityMedicineManagement/GetProductDetailsResponse.cs
@@ -10,6 +10,6 @@ namespace EquityAfia.PaymentsAndBillings.Contracts.Messages.CommodityMedicineMan
     {
         public string ProductId { get; set; }
         public int Quantity { get; set; }
-
+        public decimal Price { get; set; }
     }
 }
