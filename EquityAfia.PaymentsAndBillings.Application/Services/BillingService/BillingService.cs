e7b5b561ca40a875aed200c9b28b7f0be03a63e8
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-03 09:12:57 +0300

    deleting some codes in the billing service class

--- EquityAfia.PaymentsAndBillings.Application/Services/BillingService/BillingService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/BillingService/BillingService.cs
@@ -4,7 +4,7 @@ using EquityAfia.PaymentsAndBillings.Contracts.Billing;
 using EquityAfia.PaymentsAndBillings.Domain.Entities;
 using System;
 using System.Linq;
-using System.Threading.Tasks;
+
 
 
 
