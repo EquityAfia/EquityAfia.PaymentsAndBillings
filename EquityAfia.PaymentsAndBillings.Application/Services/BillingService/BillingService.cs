22201bc04d9c3c2d27dd8c00cc36cd82ef579c0e
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-03 09:23:32 +0300

    added a new line of code to bilingService class

--- EquityAfia.PaymentsAndBillings.Application/Services/BillingService/BillingService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/BillingService/BillingService.cs
@@ -6,7 +6,9 @@ using EquityAfia.PaymentsAndBillings.Application.Interfaces.Billing;
 namespace EquityAfia.PaymentsAndBillings.Application.Services.BillingService
 {
     public class BillingService : IBillingService
-    { }
+    {
+
+    }
 }
 
 
