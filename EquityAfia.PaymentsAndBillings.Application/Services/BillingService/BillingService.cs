64c8475b5e965eb586fa1d06c6fd19f7beda19df
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-03 09:06:21 +0300

    deleting some codes in the billing service class

--- EquityAfia.PaymentsAndBillings.Application/Services/BillingService/BillingService.cs
+++ EquityAfia.PaymentsAndBillings.Application/Services/BillingService/BillingService.cs
@@ -37,7 +37,7 @@ namespace EquityAfia.PaymentsAndBillings.Application.Services.BillingService
             }
 
             // Retrieve products from E-Pharmacy
-            var products = await _ePharmacyService.GetProductsByCustomerIdAsync(billingDto.CustomerId);
+            
             
           
             
