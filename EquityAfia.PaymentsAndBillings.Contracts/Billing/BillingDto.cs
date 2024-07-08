9f91b808720c58ae33736bd355db7ff642b69f24
Gladysrono <gladyschepkoech408@gmail.com>
2024-07-08 08:56:28 +0300

    AmountBilled: Changed back to init from set to maintain immutability. Ensured init for properties that need to be set during initialization only.

--- EquityAfia.PaymentsAndBillings.Contracts/Billing/BillingDto.cs
+++ EquityAfia.PaymentsAndBillings.Contracts/Billing/BillingDto.cs
@@ -8,7 +8,7 @@
         public required string CustomerEmail { get; init; }
         public required string CustomerPhoneNumber { get; init; }
         public int AppointmentId { get; init; }
-        public int AmountBilled { get; set; } // Changed from decimal  to int
+        public int AmountBilled { get; init; } // Changed from set to init
         public bool PayBill { get; init; }
         public required int AccNo { get; init; }
         public required string PaymentStatus { get; init; }
@@ -25,8 +25,8 @@
 
     public record ProductDto
     {
-        public required string ProductId { get; set; }
-        public int Quantity { get; set; }
-        public int Price { get; set; }
+        public required string ProductId { get; init; }
+        public int Quantity { get; init; }
+        public int Price { get; init; }
     }
 }
